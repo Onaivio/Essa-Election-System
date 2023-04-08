5a1880ac396111b4543dbe6aae38a88f5864f71d
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-04-08 21:29:28 +0100

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,57 @@
-import React, {useState} from 'react'
-import { Link, NavLink } from "react-router-dom"
-
-
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import { links } from "../data";
+import { GoThreeBars } from "react-icons/go";
+import { MdOutlineClose } from "react-icons/md";
+import "./navbar.css";
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+  const [isNavShowing, setisNavShowing] = useState(false);
 
-export default Navbar
+  // const showing = () => {
+  //      setisNavShowing = !isNavShowing
+  //      return setisNavShowing
+  // }
+
+  // const toggleNav = () => setisNavShowing(!isNavShowing);
+  const toggleIsNavShowing = () => {
+    setisNavShowing((prev) => !prev);
+  };
+
+  const setFalse = () => {
+    setisNavShowing(false);
+  };
+
+  return (
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className="logo">
+          <img src={Logo} alt="Nav logo" onClick={setFalse} />
+        </Link>
+        <ul
+          className={`nav__links ${isNavShowing ? "show__nav" : "hide__nav"}`}
+        >
+          {links.map(({ name, path }, index) => {
+            return (
+              <li key={index}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => (isActive ? "active-nav" : "")}
+                  onClick={toggleIsNavShowing}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <button className="nav__toggle-btn" onClick={toggleIsNavShowing}>
+          {isNavShowing ? <MdOutlineClose /> : <GoThreeBars />}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
