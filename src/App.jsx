c5d9952ae143668d32f28ab59d1829e762bf7d71
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-04-08 21:18:51 +0100

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Carousel from './Carousel/Carousel'
+import { Navbar } from './components'
 
 
 function App() {
 
   return (
     <BrowserRouter>
-      
+      <Navbar />
     </BrowserRouter>
     
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link, NavLink } from "react-router-dom"
+
+
 
 const Navbar = () => {
   return (
