5f56ad85184928d30600018cfb4060be8a1b996c
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-04-09 14:42:22 +0100

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import Carousel from './Carousel/Carousel'
-import { Navbar } from './components'
+import { Navbar, Footer } from './components'
 import Home from './pages/Home/Home'
+import NotFound from './pages/NotFound/NotFound'
 
 
 
 function App() {
 
   return (
+    <>
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route index element= {<Home />} />
-
+        <Route path = "*" element = {<NotFound/>} />
       </Routes>
     </BrowserRouter>
-    
+    {/* <Footer /> */}
+    </>
   )
 }
 
