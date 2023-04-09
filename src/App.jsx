4c1c5e317b6a622615ca4d7fdb8ebd394389fc61
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-04-09 14:12:46 +0100

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Carousel from './Carousel/Carousel'
+// import Carousel from './Carousel/Carousel'
 import { Navbar } from './components'
+import Home from './pages/Home/Home'
+
 
 
 function App() {
@@ -9,7 +11,10 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Carousel/>
+      <Routes>
+        <Route index element= {<Home />} />
+
+      </Routes>
     </BrowserRouter>
     
   )
